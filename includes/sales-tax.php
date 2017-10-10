195c8bdac4811c0468f61d200627be472b751cdd
Jeremy Felt <jeremy.felt@gmail.com>
2017-10-09 21:38:15 -0700

    Refactor tax rate handling and store rates as a post type
    Our tax rates are different from the rates expected by WooCommerce
    because Washington state will vary on address and postal code. We
    don't have a connection for 4 digit zip lookup, but we do have an
    API that provides tax information based on a full address.
    
    When a rate is obtained based on an address, city, and postal code,
    it is stored in cache and as a custom post type. As long as the
    customer's address stays the same, then the tax will remain
    untouched.
    
    Future work will need to be done to ensure that address changes
    work as expected.

--- includes/sales-tax.php
+++ includes/sales-tax.php
@@ -2,10 +2,10 @@
 
 namespace WSU\WooCommerce_Extended\Sales_Tax;
 
+add_action( 'init', 'WSU\WooCommerce_Extended\Sales_Tax\register_post_type' );
 add_filter( 'woocommerce_find_rates', 'WSU\WooCommerce_Extended\Sales_Tax\find_tax_rate' );
 add_filter( 'woocommerce_rate_code', 'WSU\WooCommerce_Extended\Sales_Tax\rate_code', 10, 2 );
 add_filter( 'woocommerce_rate_label', 'WSU\WooCommerce_Extended\Sales_Tax\rate_label', 10, 2 );
-add_filter( 'woocommerce_calc_shipping_tax', 'WSU\WooCommerce_Extended\Sales_Tax\calculate_shipping_tax', 10, 3 );
 
 /**
  * Show only one tax line item to the customer rather than the same tax rate
@@ -16,6 +16,182 @@ add_filter( 'woocommerce_calc_shipping_tax', 'WSU\WooCommerce_Extended\Sales_Tax
 add_filter( 'pre_option_woocommerce_tax_total_display', '__return_zero' );
 
 /**
+ * Maintain a common cache key for tax lookups.
+ *
+ * @since 0.2.0
+ *
+ * @return string
+ */
+function get_cache_group() {
+	return 'wsuwpwootax_01';
+}
+
+/**
+ * Return the slug used for the tax rate post type.
+ *
+ * @since 0.2.0
+ *
+ * @return string
+ */
+function get_post_type_slug() {
+	return 'wsu_tax_rate';
+}
+
+/**
+ * Register a post type to store tax rate look-ups for repeated use.
+ *
+ * @since 0.2.0
+ */
+function register_post_type() {
+	$labels = array(
+		'name' => 'Tax Rates',
+		'singular_name' => 'Tax Rate',
+		'add_new' => 'Add New',
+	);
+
+	$args = array(
+		'labels'             => $labels,
+		'public'             => false,
+		'publicly_queryable' => false,
+		'show_ui'            => true,
+		'show_in_menu'       => false,
+		'query_var'          => false,
+		'rewrite'            => false,
+		'has_archive'        => false,
+		'hierarchical'       => false,
+		'supports'           => array( 'title' ),
+	);
+	\register_post_type( get_post_type_slug(), $args );
+}
+
+/**
+ * Build the data necessary for making a request to the WA tax API.
+ *
+ * @since 0.2.0
+ *
+ * @param \WC_Order $order
+ *
+ * @return array
+ */
+function get_tax_request_data( $order = 0 ) {
+	if ( is_admin() && isset( $_POST['order_id'] ) && 0 === $order ) { // @codingStandardsIgnoreLine
+		$order = absint( $_POST['order_id'] );
+	}
+
+	if ( ! WC()->customer && 0 === $order ) {
+		return array();
+	}
+
+	if ( 0 !== $order ) {
+		$order = wc_get_order( $order );
+		$address = $order->get_shipping_address_1();
+		$state = $order->get_shipping_state();
+		$postcode = $order->get_shipping_postcode();
+		$city = $order->get_shipping_city();
+	} elseif ( WC()->customer && ! is_admin() ) {
+		$address = WC()->customer->get_shipping_address_1();
+		$state    = WC()->customer->get_shipping_state();
+		$postcode = WC()->customer->get_shipping_postcode();
+		$city     = WC()->customer->get_shipping_city();
+	} else {
+		return array();
+	}
+
+	// Only handle taxes for the state of Washington.
+	if ( 'WA' !== $state ) {
+		return array();
+	}
+
+	// When first building the cart, all fields are empty.
+	if ( empty( $address ) && empty( $postcode ) && empty( $city ) ) {
+		return array();
+	}
+
+	$tax_url = 'https://webgis.dor.wa.gov/webapi/addressrates.aspx';
+	$tax_url = add_query_arg( array(
+		'output' => 'text',
+		'addr' => rawurlencode( $address ),
+		'city' => rawurlencode( $city ),
+		'zip' => rawurlencode( $postcode ),
+	), $tax_url );
+
+	$lookup_key = md5( $tax_url );
+
+	return array(
+		'rate_key' => $lookup_key,
+		'request_url' => $tax_url,
+	);
+}
+
+/**
+ * Store the API data associated with an address/rate lookup.
+ *
+ * @since 0.2.0
+ *
+ * @param string $rate_key
+ * @param array  $rate_data
+ *
+ * @return int|\WP_Error
+ */
+function store_rate_data( $rate_key, $rate_data = array() ) {
+	$rate = get_page_by_title( $rate_key, OBJECT, get_post_type_slug() );
+	$rate = $rate->ID;
+
+	if ( ! $rate ) {
+		$rate = wp_insert_post( array(
+			'post_type' => get_post_type_slug(),
+			'post_title' => $rate_key,
+		) );
+	}
+
+	if ( ! is_wp_error( $rate ) && ! empty( $rate_data ) ) {
+		update_post_meta( $rate, '_wsu_woocommerce_rate_data', $rate_data );
+	}
+
+	return $rate;
+}
+
+/**
+ * Get the stored rate object for an API lookup key.
+ *
+ * @since 0.2.0
+ *
+ * @param string $rate_key
+ *
+ * @return null|\WP_Post
+ */
+function get_stored_rate( $rate_key ) {
+	$rate = get_page_by_title( $rate_key, OBJECT, get_post_type_slug() );
+
+	return $rate;
+}
+
+/**
+ * Get the data attached to a stored rate for an API lookup key.
+ *
+ * @since 0.2.0
+ *
+ * @param string $rate_key
+ *
+ * @return array
+ */
+function get_stored_rate_data( $rate_key ) {
+	$rate = get_stored_rate( $rate_key );
+
+	if ( ! $rate ) {
+		return array();
+	}
+
+	$rate_data = get_post_meta( $rate->ID, '_wsu_woocommerce_rate_data', true );
+
+	if ( ! $rate_data ) {
+		return array();
+	}
+
+	return $rate_data;
+}
+
+/**
  * Finds the tax rate for a customer's cart address using the
  * WA DOR sales tax API.
  *
@@ -23,47 +199,43 @@ add_filter( 'pre_option_woocommerce_tax_total_display', '__return_zero' );
  *
  * @return array
  */
-function find_tax_rate() {
-	if ( ! WC()->customer ) {
+function find_tax_rate( $order_id = 0 ) {
+	if ( is_array( $order_id ) ) {
+		$order_id = 0;
+	}
+
+	$request_data = get_tax_request_data( $order_id );
+
+	if ( empty( $request_data ) ) {
 		return array();
 	}
 
-	$address = WC()->customer->get_shipping_address_1();
-	$state    = WC()->customer->get_shipping_state();
-	$postcode = WC()->customer->get_shipping_postcode();
-	$city     = WC()->customer->get_shipping_city();
+	$existing = wp_cache_get( $request_data['rate_key'], get_cache_group() );
 
-	if ( 'WA' !== $state ) {
-		return array();
+	if ( $existing ) {
+		return $existing;
 	}
 
-	$matched_rates = array();
+	$matched_rate_data = get_stored_rate_data( $request_data['rate_key'] );
 
-	$tax_url = 'https://webgis.dor.wa.gov/webapi/addressrates.aspx';
-	$tax_url = add_query_arg( array(
-		'output' => 'text',
-		'addr' => urlencode( $address ),
-		'city' => urlencode( $city ),
-		'zip' => urlencode( $postcode ),
-	), $tax_url );
+	if ( ! empty( $matched_rate_data ) ) {
+		wp_cache_set( $request_data['rate_key'], $matched_rate_data, get_cache_group() );
 
-	$lookup_key = md5( $tax_url );
-
-	$existing = wp_cache_get( $lookup_key, 'wsuwp_woo_tax' );
-
-	if ( $existing && isset( $existing['matched_rates'] ) ) {
-		return $existing['matched_rates'];
+		return $matched_rate_data;
 	}
 
-	$response = wp_remote_get( $tax_url, array(
+	$response = wp_remote_get( $request_data['request_url'], array(
 		'sslverify' => false,
 	) );
+
+	$rate_id = store_rate_data( $request_data['rate_key'] );
+	$matched_rates = array();
 
 	if ( ! is_wp_error( $response ) ) {
 		$result = trim( wp_remote_retrieve_body( $response ) );
 		$result = explode( ' ', $result );
 
-		foreach( $result as $res ) {
+		foreach ( $result as $res ) {
 			if ( 0 === strpos( $res, 'Rate=' ) ) {
 				$rate = substr( $res, 5 );
 
@@ -72,7 +244,7 @@ function find_tax_rate() {
 					continue;
 				}
 
-				$matched_rates[ $lookup_key ] = array(
+				$matched_rates[ $rate_id ] = array(
 					'rate' => $rate * 100,
 					'label' => ( $rate * 100 ) . '% Sales Tax',
 					'shipping' => 'yes',
@@ -82,14 +254,14 @@ function find_tax_rate() {
 		}
 	} else {
 		$logger = new \WC_Logger();
-		$logger->add( 'wsuws-tax', 'Tax lookup failed: ' . $response->get_error_message() . ', ' . $tax_url );
+		$logger->add( 'wsuws-tax', 'Tax lookup failed: ' . $response->get_error_message() . ', ' . $request_data['request_url'] );
 	}
 
 	$exp = 0;
 
 	// If no tax rate is found using the API, use the highest WA state rate - 10.4%.
 	if ( empty( $matched_rates ) ) {
-		$matched_rates[ $lookup_key ] = array(
+		$matched_rates[ $rate_id ] = array(
 			'rate' => 10.4,
 			'label' => '10.4% (estimated) Sales Tax',
 			'shipping' => 'yes',
@@ -98,38 +270,15 @@ function find_tax_rate() {
 
 		// Estimated tax rates should not live in cache long.
 		$exp = 60;
+	} else {
+
+		// If a tax rate was found, store it persistently before caching it.
+		store_rate_data( $request_data['rate_key'], $matched_rates );
 	}
 
-	$code_storage = array(
-		'location_code' => 'US-WA-TAX-1',
-		'matched_rates' => $matched_rates,
-	);
-	wp_cache_set( $lookup_key, $code_storage, 'wsuwp_woo_tax', $exp );
+	wp_cache_set( $request_data['rate_key'], $matched_rates, get_cache_group(), $exp );
 
-	return $code_storage['matched_rates'];
-}
-
-/**
- * Calculates the sales tax on shipping costs.
- *
- * @since 0.1.1
- *
- * @param $taxes
- * @param $price
- * @param $rates
- *
- * @return array
- */
-function calculate_shipping_tax( $taxes, $price, $rates ) {
-	$rate = find_tax_rate();
-
-	if ( empty( $rate ) ) {
-		return array( 'shipping' => 0 );
-	}
-
-	$rate = reset( $rate );
-
-	return array( 'shipping' => $price * ( $rate['rate'] / 100 ) );
+	return $matched_rates;
 }
 
 /**
@@ -148,13 +297,7 @@ function rate_code( $code_string, $key ) {
 		return 'shipping';
 	}
 
-	$existing = wp_cache_get( $key, 'wsuwp_woo_tax' );
-
-	if ( $existing && isset( $existing['location_code'] ) ) {
-		return $existing['location_code'];
-	}
-
-	return $code_string;
+	return 'US-WA-TAX-1';
 }
 
 /**
@@ -170,16 +313,13 @@ function rate_code( $code_string, $key ) {
 function rate_label( $rate_name, $key ) {
 	if ( 'shipping' === $key ) {
 		$rates = find_tax_rate();
-		foreach( $rates as $key => $rate ) {
-			return $rates[ $key ]['label'] . " (Shipping)";
+		foreach ( $rates as $key => $rate ) {
+			return $rates[ $key ]['label'] . ' (Shipping)';
 		}
 	}
 
-	$existing = wp_cache_get( $key, 'wsuwp_woo_tax' );
+	$rate = find_tax_rate();
+	$rate = reset( $rate );
 
-	if ( $existing && isset( $existing['matched_rates'] ) ) {
-		return $existing['matched_rates'][ $key ]['label'];
-	}
-
-	return $rate_name;
+	return $rate['label'];
 }
